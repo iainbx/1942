907eb20216e1d779ab75ebfa73ad320806ca1d16
iainbx <iainbaxter@msn.com>
2015-01-12 22:34:02 -0500

    wip

--- 1942/game.js
+++ 1942/game.js
@@ -9,10 +9,65 @@ BasicGame.Game.prototype = {
 
     this.sea = this.add.tileSprite(0, 0, 800, 600, 'sea');
 
+    this.player = this.add.sprite(400, 550, 'player');
+    this.player.anchor.setTo(0.5, 0.5);
+    this.player.animations.add('fly', [0, 1, 2], 20, true);
+    this.player.play('fly');
+    this.physics.enable(this.player, Phaser.Physics.ARCADE);
+    this.player.speed = 300;
+    this.player.body.collideWorldBounds = true;
+
+
+    this.enemy = this.add.sprite(400, 200, 'greenEnemy');
+    this.enemy.animations.add('fly', [0, 1, 2], 20, true);
+    this.enemy.play('fly');
+    this.enemy.anchor.setTo(0.5, 0.5);
+    this.physics.enable(this.enemy, Phaser.Physics.ARCADE);
+
+
+
+    this.bullets = [];
+
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
   },
 
   update: function () {
     //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+      this.sea.tilePosition.y += 0.2;
+
+      for (var i = 0; i < this.bullets.length; i++) {
+          this.physics.arcade.overlap(
+            this.bullets[i], this.enemy, this.enemyHit, null, this
+          );
+      }
+
+      this.player.body.velocity.x = 0;
+      this.player.body.velocity.y = 0;
+
+      if (this.cursors.left.isDown) {
+          this.player.body.velocity.x = -this.player.speed;
+      } else if (this.cursors.right.isDown) {
+          this.player.body.velocity.x = this.player.speed;
+      }
+
+      if (this.cursors.up.isDown) {
+          this.player.body.velocity.y = -this.player.speed;
+      } else if (this.cursors.down.isDown) {
+          this.player.body.velocity.y = this.player.speed;
+      }
+
+      if (this.input.activePointer.isDown &&
+        this.physics.arcade.distanceToPointer(this.player) > 15) {
+          this.physics.arcade.moveToPointer(this.player, this.player.speed);
+      }
+
+      if (this.input.keyboard.isDown(Phaser.Keyboard.Z) ||
+    this.input.activePointer.isDown) {
+          this.fire();
+      }
+
   },
 
   quitGame: function (pointer) {
@@ -23,6 +78,23 @@ BasicGame.Game.prototype = {
     //  Then let's go back to the main menu.
     this.state.start('MainMenu');
 
-  }
+  },
+  fire: function() {
+           var bullet = this.add.sprite(this.player.x, this.player.y - 20, 'bullet');
+           bullet.anchor.setTo(0.5, 0.5);
+           this.physics.enable(bullet, Phaser.Physics.ARCADE);
+           bullet.body.velocity.y = -500;
+           this.bullets.push(bullet);
+         },
+
+
+    enemyHit: function (bullet, enemy) {
+         bullet.kill();
+         enemy.kill();
+         var explosion = this.add.sprite(enemy.x, enemy.y, 'explosion');
+         explosion.anchor.setTo(0.5, 0.5);
+         explosion.animations.add('boom');
+         explosion.play('boom', 15, false, true);
+    }
 
 };
